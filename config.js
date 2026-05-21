f484fc6a7fa2ef8e1da5aef7f694432e09040459
Ivan Bernabeu Romero <bernaveuromeroivan@fpllefia.com>
2026-05-21 18:24:35 +0200

    IA5 acabada 2

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://eaqulnvjzguvcyxyhqef.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhcXVsbnZqemd1dmN5eHlocWVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNzk1NTIsImV4cCI6MjA5NDk1NTU1Mn0.UL_DKUmjJvhcLplHhid4IrTwDVZxcMsVU2BQMCq6TMw"
+};
